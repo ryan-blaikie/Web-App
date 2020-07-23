85364009c18113ee552c3314879b7f1b3eff1896
Ryan Blaikie <ryan.blaikie@gmail.com>
2020-07-24 10:50:46 +1200

    Updated Search Bar w/o Dates

--- src/Components/SearchBarComponent/SearchBar.tsx
+++ src/Components/SearchBarComponent/SearchBar.tsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Grid, TextField } from '@material-ui/core';
+import { IUserInput } from '../../Common/Interfaces'
 import './SearchBar.css';
 
-function SearchBar() {
-    return (
-        <div>
-        </div>
-    )
+interface ISearchBarProps {
+    SetUserInput: (a: IUserInput) => void;
+}
+
+
+function SearchBar(props: ISearchBarProps) {
+
+    const [SearchQuery, setSearchQuery] = useState<string | null>("");
+    const handleSearchQueryChange = (s: string | null) => {
+        setSearchQuery(s);
+    }
+    const [HasFocus, setHasFocus] = useState<boolean>(false);
+
+    const handleSubmit = () => {
+        console.log(SearchQuery);
+
+        if (SearchQuery?.length !== 0 && SearchQuery !== null && SearchQuery !== "") {
+            let UserInput: IUserInput = {
+                SearchQuery: SearchQuery,
+            }
+            props.SetUserInput(UserInput);
+        } else {
+            setHasFocus(true);
+        }
+    }
+    
+    return <div className="SearchBarContainer">
+        <Grid container spacing={3}>
+            <Grid item xs={6} sm={3}>
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Search"
+                    variant="outlined"
+                    error={HasFocus && SearchQuery === ""}
+                    onClick={() => setHasFocus(true)}
+                    value={SearchQuery}
+                    onChange={e => handleSearchQueryChange(e.target.value)}
+                />
+            </Grid>
+            <Grid item xs={6} sm={3}>
+                <Button variant="contained" color="primary" onClick={handleSubmit}>
+                    Submit
+                </Button>
+            </Grid>
+        </Grid>
+    </div>
 }
 
 export default SearchBar
